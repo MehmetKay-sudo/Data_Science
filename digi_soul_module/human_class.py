6459092b52e576d54472bb50cc2cc9724c076ea1
Mehmet Kücük <mehmet.kay.programming@gmail.com>
2023-12-23 20:12:05 +0100

    body as a program

---
+++ digi_soul_module/human_class.py
@@ -0,0 +1,24 @@
+# object.attribute
+
+class human():
+    def __init__(self):
+        self.human = human
+
+    def weight(self, kg):
+        self.weight = weight
+        self.kg = kg
+        return self.weight
+
+    def height(self, cm):
+        self.height = height
+        self.cm = cm
+        return self.height
+
+    def mind(self, synapse):
+        self.mind = mind
+        self.synapse = synapse
+        return self.mind
+
+#human.weight(99)
+#human.height(184)
+#human.mind(?)
